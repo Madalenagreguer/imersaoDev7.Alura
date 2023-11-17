d140c17efe6731cd42c4f5db336698f7200c75f7
Madalena Greguer <greguer73@gmail.com>
2023-11-17 17:56:50 -0300

    Publicar filmes favoritos
    Além de imagens , trabalhando com urls e alguns conceitos.

---
+++ _aluraflixprojeto/README.md
@@ -0,0 +1,6 @@
+# _AluraFlix - Projeto
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Madalena-Greguer/pen/WNPXmVP](https://codepen.io/Madalena-Greguer/pen/WNPXmVP).
+
+Lista de filmes preferidos.
+Simplificando o código com uma lista [array].
---
+++ _aluraflixprojeto/dist/script.js
@@ -0,0 +1,6 @@
+var listaFilmes = ['https://cdn.kobo.com/book-images/7b0e1631-9550-459a-9914-ab53b5fdb82e/1200/1200/False/orgulho-e-preconceito-19.jpg','https://br.web.img3.acsta.net/medias/nmedia/18/87/32/87/19874367.jpg','https://m.media-amazon.com/images/I/91zb3WEGKvL._AC_UF1000,1000_QL80_.jpg','https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/09/12/1412980909-4fefb2a3e5a9d3cf2904f90eea35b8a1.jpg'
+  
+];
+ for (var i = 0; i < listaFilmes.length; i++){
+   document.write("<img src=" + listaFilmes[i] + ">");
+ }
